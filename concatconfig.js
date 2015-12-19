de98ab2bcb6b1b6fa2b63f4c766474c9f83d3b97
motoki-shun <motoki-shun@kayac.com>
2015-12-20 00:58:13 +0900

    Tetris3dクラス化

--- concatconfig.js
+++ concatconfig.js
@@ -2,6 +2,7 @@ module.exports = {
   files: [
     'bower_components/jquery/dist/jquery.min.js', 
     'src/js/Util.js', 
+    'src/js/Tetris3d.js', 
     'src/js/main.js'
   ],
   dest: 'script.js'
