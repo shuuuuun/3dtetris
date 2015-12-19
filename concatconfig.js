e95bf033fe725b6e9e42bf06213327ef25538293
motoki-shun <motoki-shun@kayac.com>
2015-12-20 01:22:08 +0900

    threejsをbowerで入れて最新にしてみる

--- concatconfig.js
+++ concatconfig.js
@@ -1,6 +1,7 @@
 module.exports = {
   files: [
     'bower_components/jquery/dist/jquery.min.js', 
+    'bower_components/three.js/build/three.min.js', 
     'src/js/Util.js', 
     'src/js/Tetris3d.js', 
     'src/js/main.js'
