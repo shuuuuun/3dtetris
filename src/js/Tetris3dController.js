4ea1c6006d6b8e77c94f5fc7228c702418b5e284
motoki-shun <motoki-shun@kayac.com>
2016-02-04 23:05:25 +0900

    Tetris3dController

---
+++ src/js/Tetris3dController.js
@@ -0,0 +1,101 @@
+import $ from 'jquery';
+import EE2 from 'eventemitter2';
+import Tetris3dCONST from './Tetris3dCONST';
+
+const CONST = Tetris3dCONST;
+
+class Tetris3dController extends EE2.EventEmitter2 {
+  constructor(model, view) {
+    super();
+    
+    this.model = model;
+    this.view = view;
+    
+    this.eventify();
+  };
+  
+  newGame() {
+    this.model.initGame();
+    this.model.startGame();
+  };
+  
+  eventify() {
+    // console.log(this.model.currentBlock);
+    this.model.on('gamestart', () => {});
+    this.model.on('newblockcreated', () => {});
+    this.model.on('currentblockcreated', () => {});
+    this.model.on('nextblockcreated', () => {});
+    this.model.on('gameover', () => {});
+    this.model.on('tick', () => {
+      console.log(this.model.currentBlock);
+      // this.view.drawBlock(0, 0, 0, 0);
+    });
+    this.model.on('gamequit', () => {});
+    this.model.on('freeze', () => {});
+    this.model.on('clearline', (filledRowList) => {});
+  };
+  
+  setBlurEvent() {
+    $(window).on('blur', () => {
+        this.pauseGame();
+    }).on('focus', () => {
+        this.resumeGame();
+    });
+  };
+  
+  setKeyEvent() {
+    $(document).on('keydown', (evt) => {
+      if (typeof this.KEYS[evt.keyCode] === 'undefined') return;
+      evt.preventDefault();
+      this.moveBlock(this.KEYS[evt.keyCode]);
+    });
+  };
+  
+  setTouchEvent() {
+    var touch = new TouchController(this.$cnvs);
+    var touchStartX;
+    var touchStartY;
+    var isTap = false;
+    var isFreeze = false;
+    
+    touch.on('touchstart', (evt, info) => {
+      touchStartX = info.touchStartX;
+      touchStartY = info.touchStartY;
+      isTap = true;
+      isFreeze = false;
+    });
+    touch.on('touchmove', (evt, info) => {
+      // var blockMoveX = (info.moveX / this.BLOCK_SIZE) | 0;
+      var moveX  = info.touchX - touchStartX;
+      var moveY  = info.touchY - touchStartY;
+      var blockMoveX = (moveX / this.BLOCK_SIZE) | 0;
+      var blockMoveY = (moveY / this.BLOCK_SIZE) | 0;
+      
+      if (isFreeze) return;
+      
+      // 1マスずつバリデーション（すり抜け対策）
+      while (!!blockMoveX) {
+        var sign = blockMoveX / Math.abs(blockMoveX); // 1 or -1
+        if (!this.valid(sign, 0)) break;
+        this.currentX += sign;
+        blockMoveX -= sign;
+        touchStartX = info.touchX;
+      }
+      while (blockMoveY > 0) {
+        if (!this.valid(0, 1)) break;
+        this.currentY++;
+        blockMoveY--;
+        touchStartY = info.touchY;
+      }
+      isTap = false;
+    });
+    touch.on('touchend', (evt, info) => {
+      if (!!isTap) this.moveBlock('rotate');
+    });
+    this.on('freeze', () => {
+      isFreeze = true;
+    });
+  };
+}
+
+module.exports = Tetris3dController;
--- src/js/main.js
+++ src/js/main.js
@@ -4,21 +4,24 @@ import Util from './Util';
 import Tetris3d from './Tetris3d';
 import Tetris3dView from './Tetris3dView';
 import Tetris3dModel from './Tetris3dModel';
+import Tetris3dController from './Tetris3dController';
 
 // const tetris = new Tetris3d();
 const tetris3dModel = new Tetris3dModel();
-const tetrisView = new Tetris3dView();
+const tetris3dView = new Tetris3dView();
+const tetris3dController = new Tetris3dController(tetris3dModel, tetris3dView);
 
 class Main {
   constructor() {
   }
   exec() {
     // tetris.init();
-    tetris3dModel.newGame();
-    tetrisView.init();
-    tetrisView.start();
-    tetrisView.drawBlock(0, 0, 0, 0);
-    tetrisView.drawBlock(0, 0, 1, 1);
+    // tetris3dModel.newGame();
+    tetris3dView.init();
+    tetris3dView.start();
+    tetris3dView.drawBlock(0, 0, 0, 0);
+    tetris3dView.drawBlock(0, 0, 1, 1);
+    tetris3dController.newGame();
   }
 }
 
