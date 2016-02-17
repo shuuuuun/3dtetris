9fce7580f5fc23dcb95e7a478ecc2307154b910f
motoki-shun <motoki-shun@kayac.com>
2016-02-18 01:38:20 +0900

    ボタンを付ける

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 // import Util from './Util';
 import Tetris3d from './Tetris3d';
 import Tetris3dView from './Tetris3dView';
@@ -9,6 +9,19 @@ import Tetris3dController from './Tetris3dController';
 const tetris3dModel = new Tetris3dModel();
 const tetris3dView = new Tetris3dView();
 const tetris3dController = new Tetris3dController(tetris3dModel, tetris3dView);
+
+const $switchCamera = $('.js-switch-camera');
+const $switchBlock = $('.js-switch-block');
+const $btns = $switchCamera.add($switchBlock);
+
+$switchCamera.on('click', function(){
+  $btns.removeClass('is-active');
+  $(this).addClass('is-active');
+});
+$switchBlock.on('click', function(){
+  $btns.removeClass('is-active');
+  $(this).addClass('is-active');
+});
 
 class Main {
   constructor() {
