28e4d1838d88d8e7b32bd3ac3eab42983e654a00
motoki-shun <motoki-shun@kayac.com>
2016-04-03 01:41:41 +0900

    Viewの要らないコードを思いきり削除

--- src/js/Tetris3dView.js
+++ src/js/Tetris3dView.js
@@ -16,11 +16,6 @@ export default class Tetris3dView {
     this.CAMERA_POSITION = new THREE.Vector3(CONST.CENTER_X, -100, this.CAMERA_DISTANCE_DEFAULT);
     this.CAMERA_NEAR = 1;
     this.CAMERA_FAR = 100000;
-    
-    // this.radius = 150;
-    // this.theta = 45;
-    // this.phi = 0;
-    // this.radiusStep = 10;
   }
   
   init() {
@@ -41,46 +36,14 @@ export default class Tetris3dView {
     
     // camera ------------------------------
     this.perscamera = new THREE.PerspectiveCamera( 45, CONST.WIDTH / CONST.HEIGHT, this.CAMERA_NEAR, this.CAMERA_FAR ); // fov(視野角), aspect, near, far
-    // this.orthocamera = new THREE.OrthographicCamera( -CONST.WIDTH / 2, CONST.WIDTH / 2, CONST.HEIGHT / 2, -CONST.HEIGHT / 2, this.CAMERA_NEAR, this.CAMERA_FAR ); // left, right, top, bottom, near, far
     this.orthocamera = new THREE.OrthographicCamera( -window.innerWidth / 2, window.innerWidth / 2, window.innerHeight / 2, -window.innerHeight / 2, this.CAMERA_NEAR, this.CAMERA_FAR ); // left, right, top, bottom, near, far
     this.cubecamera = new THREE.CubeCamera( this.CAMERA_NEAR, this.CAMERA_FAR, 128 ); // near, far, cubeResolution
     this.setCamera();
-    /*
-    this.camera = this.perscamera;
-    // this.camera = this.orthocamera;
-    // this.camera = new THREE.Camera();
-    // this.camera.clone(this.perscamera);
-    this.camera.position.set(2000, CONST.CENTER_Y, 2000);
-    // this.camera.position.set(this.CAMERA_POSITION);
-    // this.camera.position.add(this.CAMERA_POSITION);
-    // this.camera.position.copy(this.CAMERA_POSITION);
-    // this.camera.position.addVectors(this.ZERO_VECTOR, this.CAMERA_POSITION);
-    // this.camera.position = this.CAMERA_POSITION;
-    this.camera.up.set(0, -1, 0); // y down
-    // this.camera.lookAt(this.CENTER_VECTOR);
-    // this.camera.lookAt(CONST.CENTER_X, CONST.CENTER_Y, CONST.CENTER_Z);
-    // let lookatVector = new THREE.Vector3().subVectors(this.CENTER_VECTOR, this.camera.position);
-    // let lookatVector = new THREE.Vector3().addVectors(this.CENTER_VECTOR, this.camera.position);
-    // this.camera.lookAt(lookatVector);
-    // this.camera.lookAt(0,1,0);
-    // this.camera.lookAt(new THREE.Vector3(0,1,0));
-    // console.log(lookatVector, this.camera.getWorldDirection());
     
     
     // controls ------------------------------
-    this.controls = new THREE.OrbitControls(this.camera);
-    // this.controls.center.set(CONST.CENTER_X, 0, CONST.CENTER_Z);
-    // this.controls.center = this.CENTER_VECTOR;
-    // this.controls.center.set(this.CENTER_VECTOR);
-    // this.controls.center.set(CONST.CENTER_X, CONST.CENTER_Y, CONST.CENTER_Z);
-    // this.controls.target.set(CONST.CENTER_X, CONST.CENTER_Y, CONST.CENTER_Z);
-    // this.controls.target = new THREE.Vector3(CONST.CENTER_X, CONST.CENTER_Y, CONST.CENTER_Z);
-    this.controls.target = this.CENTER_VECTOR;
-    // this.controls.target.set(this.CENTER_VECTOR);
-    // this.controls.noKeys = true;
-    this.controls.enableKeys = false;
-    this.controls.update();
-    */
+    // this.startControls();
+    
     
     // axis ------------------------------
     const axis = new THREE.AxisHelper(this.CAMERA_FAR);
@@ -128,14 +91,6 @@ export default class Tetris3dView {
     }
     
     
-    // plane ------------------------------
-    // plane = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000 ), new THREE.MeshBasicMaterial() );
-    // plane.rotation.x = - Math.PI / 2;
-    // plane.visible = false;
-    // this.scene.add( plane );
-    // objects.push( plane );
-    
-    
     // Lights ------------------------------
     const ambientLight = new THREE.AmbientLight( 0x606060 );
     this.scene.add( ambientLight );
@@ -144,26 +99,8 @@ export default class Tetris3dView {
     this.scene.add( directionalLight );
     
     
-    // picking ------------------------------
-    // projector = new THREE.Projector();
-    
-    
     // mouse ------------------------------
     this.mouse2D = new THREE.Vector3( 0, 10000, 0.5 );
-    
-    
-    // roll-over helpers ------------------------------
-    // rollOverGeo = new THREE.BoxGeometry( CONST.VOXEL_SIZE, CONST.VOXEL_SIZE, CONST.VOXEL_SIZE );
-    // rollOverMaterial = new THREE.MeshBasicMaterial( { color: 0xff0000, opacity: 0.5, transparent: true } );
-    // rollOverMesh = new THREE.Mesh( rollOverGeo, rollOverMaterial );
-    // this.scene.add( rollOverMesh );
-    
-    
-    // stats ------------------------------
-    // stats = new Stats();
-    // stats.domElement.style.position = 'absolute';
-    // stats.domElement.style.top = '0px';
-    // container.appendChild( stats.domElement );
     
     
     // cubes ------------------------------
@@ -184,7 +121,6 @@ export default class Tetris3dView {
   setSize() {
     this.width = window.innerWidth || CONST.WIDTH;
     this.height = window.innerHeight || CONST.HEIGHT;
-    // alert(window.innerWidth + ',' + CONST.WIDTH);
     this.camera.aspect = this.width / this.height;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize( this.width, this.height );
@@ -194,22 +130,18 @@ export default class Tetris3dView {
     switch (code) {
       case 'ortho1':
         this.camera = this.orthocamera;
-        // this.camera.clone(this.orthocamera);
         this.camera.position.set(CONST.CENTER_X, CONST.CENTER_Y, this.CAMERA_DISTANCE_DEFAULT);
         break;
       case 'ortho2':
         this.camera = this.orthocamera;
-        // this.camera.clone(this.orthocamera);
         this.camera.position.set(this.CAMERA_DISTANCE_DEFAULT, CONST.CENTER_Y, CONST.CENTER_Z);
         break;
       case 'ortho3':
         this.camera = this.orthocamera;
-        // this.camera.clone(this.orthocamera);
         this.camera.position.set(CONST.CENTER_X, -1000, CONST.CENTER_Z);
         break;
       default: // 'pers'
         this.camera = this.perscamera;
-        // this.camera.clone(this.perscamera);
         this.camera.position.copy(this.CAMERA_POSITION);
         break;
     }
@@ -230,89 +162,27 @@ export default class Tetris3dView {
     // this.controls = new THREE.OrbitControls(this.camera);
     // this.controls.target = this.CENTER_VECTOR;
     // this.controls.enableKeys = false;
-    // // this.controls.reset();
     // this.controls.update();
     this.controls = new THREE.EditorControls(this.camera);
     this.controls.center = this.CENTER_VECTOR;
   }
   
   checkCameraDirection() {
-  // checkForward() {
-    // const pos = this.camera.position.clone().normalize();
-    // const cameraVector = new THREE.Vector3().subVectors(this.camera.position, this.CENTER_VECTOR);
-    // const cameraVector = this.camera.position.clone().sub(this.CENTER_VECTOR).normalize();
     const cameraVector = this.camera.position.clone().sub(this.CENTER_VECTOR);
     let direction = {
       x: Math.sign(Math.abs(cameraVector.x) > Math.abs(cameraVector.z) ? cameraVector.x : 0),
       y: Math.sign(-cameraVector.y),
       z: Math.sign(Math.abs(cameraVector.z) > Math.abs(cameraVector.x) ? cameraVector.z : 0),
     };
-    // console.log(cameraVector.toArray(), direction);
     return direction;
   };
   
-  getCameraPositoin() {
-    return this.camera.position;
-  }
-  
-  setCameraPositoin(position) {
-    this.camera.position = position;
-  }
-  
-  moveCameraPositoin(distance) {
-    const distanceVector = new THREE.Vector3().add(distance);
-    const position = this.camera.position.sub(distanceVector);
-    console.log(distanceVector, position);
-    // this.setCameraPositoin(position);
-  }
-  
-  rotateCamera(distance) {
-  }
-  rotateCameraLeft(angle) {
-    this.thetaDelta -= angle;
-  }
-  rotateCameraUp(angle) {
-    this.phiDelta -= angle;
-  }
-  
   tick() {
     this.framecount++;
-    
-    // this.controls.update();
-    
-    // this.radius += this.radiusStep;
-    // this.phi++;
-    // this.camera.position.x = this.radius * Math.sin(this.theta /180 * Math.PI) * Math.sin(this.phi /180 * Math.PI); // 極座標変換
-    // this.camera.position.y = this.radius * Math.sin(this.theta /180 * Math.PI) * Math.cos(this.phi /180 * Math.PI);
-    // this.camera.position.z = this.radius * Math.cos(this.theta /180 * Math.PI);
-    // this.camera.lookAt( this.scene.position );
-    // if (this.radius < this.MIN_RADIUS || this.radius > this.MAX_RADIUS) this.radiusStep *= -1;
-    
-    // this.camera.position.x += ( this.mouseX - this.camera.position.x ) * 0.005;
-    // this.camera.position.y += ( - this.mouseY - this.camera.position.y ) * 0.005;
-    // this.camera.lookAt( this.scene.position );
-    
-    // this.camera.position.z = this.r * Math.sin(this.theta /180 * Math.PI) * Math.cos(this.phi /180 * Math.PI);
-    // this.camera.position.x = this.r * Math.sin(this.theta /180 * Math.PI) * Math.sin(this.phi /180 * Math.PI);
-    // this.camera.position.y = this.r * Math.cos(this.theta /180 * Math.PI);
-    // this.camera.position.x = 1400 * Math.sin( THREE.Math.degToRad( this.theta ) );
-    // this.camera.position.y = 1400 * Math.tan( THREE.Math.degToRad( thetaY ) );
-    // this.camera.position.z = 1400 * Math.cos( THREE.Math.degToRad( this.theta ) );
-    // this.camera.position.set(0,100,-500);
-    // this.camera.lookAt( this.scene.position );
   }
   
   render() {
     this.renderer.render( this.scene, this.camera );
-    
-    this.renderBoard();
-    this.renderCurrentBlock();
-  }
-  
-  renderBoard() {
-  }
-  
-  renderCurrentBlock() {
   }
   
   drawCurrentBlock(block) {
