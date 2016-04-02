4f743954a5ca9c7651832e5a14414e12b1e0c4b5
motoki-shun <motoki-shun@kayac.com>
2016-04-03 01:11:11 +0900

    そしたらimportでもいけた

--- src/js/Tetris3dView.js
+++ src/js/Tetris3dView.js
@@ -1,8 +1,7 @@
 // import THREE from 'three.js';
 // global.THREE = require('three.js'); // global === window
-// import 'three.js/examples/js/controls/OrbitControls.js'; // これじゃだめ
-require('three.js/examples/js/controls/OrbitControls.js');
-// require('three.js/examples/js/controls/EditorControls.js');
+import 'three.js/examples/js/controls/OrbitControls.js';
+// import 'three.js/examples/js/controls/EditorControls.js';
 import './lib/EditorControls2';
 import Tetris3dCONST from './Tetris3dCONST';
 
