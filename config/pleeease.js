459cdc9baf56f30faeec9e93425b8d776c105060
motoki-shun <motoki-shun@kayac.com>
2016-04-29 19:03:10 +0900

    autoprefixerの指定を簡略化

--- config/pleeease.js
+++ config/pleeease.js
@@ -1,15 +1,9 @@
 module.exports = {
   autoprefixer: {
     browsers: [
-      'ie >= 10',
-      'ie_mob >= 10',
-      'ff >= 30',
-      'chrome >= 34',
-      'safari >= 7',
-      'opera >= 23',
-      'ios >= 7',
-      'android >= 4.4',
-      'bb >= 10',
+      'last 2 versions',
+      '> 5%',
+      '> 5% in JP',
     ],
   },
   minifier: true,
