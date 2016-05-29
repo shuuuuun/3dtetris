52dec00ba06ec34bde0667f8304402addcd93a20
motoki-shun <motoki-shun@kayac.com>
2016-05-29 10:39:29 +0900

    update SoundController

--- src/js/SoundController.js
+++ src/js/SoundController.js
@@ -3,12 +3,9 @@ import Cookies from 'js-cookie';
 export default class SoundController {
     constructor(opts = {}) {
         
-        this.COOKIE_NAME = opts.COOKIE_NAME || "isMute";
+        this.COOKIE_NAME = opts.COOKIE_NAME || 'isMute';
         this.EXPIRES_DATE = opts.EXPIRES_DATE;
         this.disableBlurPause = !!opts.disableBlurPause;
-        this.toggleSelector = opts.toggleSelector;
-        
-        this.SE = {};
         
         this.audio = new Audio(opts.src);
         this.audio.volume = 0.2;
@@ -18,10 +15,6 @@ export default class SoundController {
     }
     
     initListeners() {
-        $(this.toggleSelector).on('click', () => {
-            this.toggleMute().pause().play();
-        });
-
         if (!this.disableBlurPause) {
             $(window).on('blur', () => {
                 this.pause();
@@ -70,30 +63,13 @@ export default class SoundController {
         return this;
     }
     
-    setSE(key, src) {
-        // if (Const.IS_TOUCH) return this;
-        this.SE[key] = new Audio(src);
-        return this;
-    }
-    
-    playSE(key) {
-        if (this.audio.muted) return this;
-        // if (Const.IS_TOUCH) return this;
-        
-        let se = this.SE[key];
-        if (!se) return this;
-        
-        se.play();
-        return this;
-    }
-    
     setCookie(value) {
         return Cookies.set(this.COOKIE_NAME, value, { expires: this.EXPIRES_DATE });
     }
     
     getCookie() {
         let value = Cookies.get(this.COOKIE_NAME);
-        return (value === "true");
+        return (value === 'true');
     }
     
     hasCookie() {
