fef5c8567582bbde440652420e79f2a31a7f9484
motoki-shun <motoki-shun@kayac.com>
2016-10-08 20:52:53 +0900

    add bin/deploy.sh

---
+++ bin/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+ghpages_dir="tmp/gh-pages"
+remote="git@github.com:shuuuuun/tetricus.git"
+url=$(echo $remote | perl -0ne 'print "https://$1.github.io/$2/" if (/[https?:\/\/|git\@]github\.com[\/|:]([^\/]+)\/([^\/]+)\.git/)')
+
+has_dir=$([[ -e $ghpages_dir ]]; echo $?)
+
+mkdir -pv $ghpages_dir
+cd $ghpages_dir
+
+if [[ $has_dir == 1 ]]; then
+  git clone -b gh-pages --depth 1 $remote .
+  git fetch --depth 1 origin master:refs/remotes/origin/master
+fi
+
+git pull
+# git pull origin gh-pages
+# git fetch origin master
+git checkout origin/master public
+cp -rf public/* ./
+rm -rf public
+git add --all
+git commit -m 'update'
+git push origin gh-pages
+
+echo $url
+
