f5d95ce5fd8a1ecfc026ce63beb0d7280ac1a4d3
motoki-shun <motoki-shun@kayac.com>
2016-10-22 10:43:25 +0900

    bin/deploy.sh:add comment

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -6,22 +6,32 @@ url=$(echo $remote | perl -0ne 'print "https://$1.github.io/$2/" if (/[https?:\/
 
 has_dir=$([[ -e $ghpages_dir ]]; echo $?)
 
+echo "=============== cd $ghpages_dir ==============="
 mkdir -pv $ghpages_dir
 cd $ghpages_dir
 
 if [[ $has_dir == 1 ]]; then
+  echo "=============== clone repository ==============="
   git clone -b gh-pages --depth 1 $remote .
   git fetch --depth 1 origin master:refs/remotes/origin/master
 fi
 
+echo "=============== pull & fetch ==============="
 git pull origin gh-pages
 git fetch origin master:refs/remotes/origin/master
+
+echo "=============== checkout public from master ==============="
 git checkout origin/master public
 cp -rf public/* ./
 rm -rf public
+
+echo "=============== commit ==============="
 git add --all
 git commit -m 'update'
+
+echo "=============== push ==============="
 git push origin gh-pages
 
-echo $url
+echo ""
+echo "URL: $url"
 
