5a73bfe083febf980966b7f8631273cf69569303
motoki-shun <motoki-shun@kayac.com>
2016-10-25 23:42:20 +0900

    watchify

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -42,6 +42,7 @@ import uglify from 'gulp-uglify';
 import gutil from 'gulp-util';
 import rename from 'gulp-rename';
 import browserify from 'browserify';
+import watchify from 'watchify';
 import source from 'vinyl-source-stream';
 import buffer from 'vinyl-buffer';
 import babelify from 'babelify';
@@ -137,6 +138,10 @@ gulp.task('browserify', () => {
   .pipe(notify('browserify build succeeded!!'));
 });
 
+gulp.task('watchify', () => {
+  bundleJs(true);
+});
+
 gulp.task('lint', () => {
   gulp.src([GLOB_JS, GLOB_UNBUILD])
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
@@ -144,3 +149,27 @@ gulp.task('lint', () => {
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
+
+
+function bundleJs(watching = false) {
+  const b = browserify({
+    entries: config.browserify.entries,
+    transform: [babelify],
+    plugin: watching ? [watchify] : null,
+  });
+  b.on('update', () => {
+    bundler();
+    console.log('scripts rebuild');
+  });
+  function bundler() {
+    return b.bundle()
+      .on('error', (err) => {
+        console.log(err.message);
+      })
+      .pipe(source(config.browserify.dest))
+      .pipe(buffer())
+      .pipe(gulpif(!gutil.env.develop, uglify({ preserveComments: 'some' }))) // developモードではminifyしない
+      .pipe(gulp.dest(DEST_JS));
+  }
+  return bundler();
+}
